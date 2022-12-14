9a4c813ea75accdf32ef2fd450c7e0693de350fa
angela <angelabecerraforero@gmail.com>
2022-12-14 09:45:33 -0500

    A web Kanban board is created with Angular and Firebase. The app has three categories of tasks: pending, in progress and completed tasks. With drag-and-drop functionality, we will be able to create, delete tasks and transfer them from one category to another

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-950a0',
+    appId: '1:176228909999:web:f8a9dc81549c6e6ad8255a',
+    storageBucket: 'kanbanfire-950a0.appspot.com',
+    apiKey: 'AIzaSyDjeb6fQ1s-xpdSfVRQBadJrimuUffX89Q',
+    authDomain: 'kanbanfire-950a0.firebaseapp.com',
+    messagingSenderId: '176228909999',
+    measurementId: 'G-EXPT54D0JJ',
+  },
   production: true
 };
